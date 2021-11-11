5a2d6e24255aa681df3684bedb3e7c9faadd46d3
darthvaderson <first_number@naver.com>
2021-11-11 16:42:35 +0900

    user Repository 부분 수정

--- src/main/java/com/koscom/hackathon/domain/user/UserRepository.java
+++ src/main/java/com/koscom/hackathon/domain/user/UserRepository.java
@@ -1,8 +1,10 @@
 package com.koscom.hackathon.domain.user;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.Optional;
 
-public interface UserRepository {
+public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
 
