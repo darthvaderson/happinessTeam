d0c1f57b21310a2be52d0d4ffc8bb73ac17c5ad0
darthvaderson <first_number@naver.com>
2021-11-11 16:37:47 +0900

    user Repository 부분 수정

--- src/main/java/com/koscom/hackathon/web/IndexController.java
+++ src/main/java/com/koscom/hackathon/web/IndexController.java
@@ -1,11 +1,24 @@
 package com.koscom.hackathon.web;
 
 
+import com.koscom.hackathon.config.auth.dto.SessionUser;
+import com.koscom.hackathon.domain.user.User;
+import com.koscom.hackathon.domain.user.UserRepository;
+import com.koscom.hackathon.web.dto.UserInfo;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import javax.servlet.http.HttpSession;
+
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
+
+    private final HttpSession httpSession;
+    private final UserRepository userRepository;
 
     @GetMapping("/main")
     public String index() {
@@ -14,6 +27,15 @@ public class IndexController {
 
     @GetMapping("/index")
     public String index2(){
+
+        return "index";
+    }
+
+    @PostMapping("/index")
+    public String index_userInfo(@RequestBody Object object){
+
+        SessionUser sessionUser = (SessionUser) httpSession.getAttribute("user");
+        User user = userRepository.findByEmail(sessionUser.getEmail()).orElse(null);
 
 
         return "index";
---
+++ src/main/java/com/koscom/hackathon/web/dto/UserInfo.java
@@ -0,0 +1,14 @@
+package com.koscom.hackathon.web.dto;
+
+
+import lombok.Data;
+
+@Data
+public class UserInfo {
+
+    private String currentAsset;
+    private String annualInvest;
+    private String workingYear;
+    private String riskTolerance;
+
+}
